8e9865dfb2808b0fda6069ecba226eb38cd00145
Nikhil Vaidyar <nikhil.vaidyar@biofourmis.com>
2022-08-07 14:28:20 +0530

    Updated position of code line to achieve release mode feature of gin framework

--- main.go
+++ main.go
@@ -18,20 +18,24 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// To initialize router with gin
-	router := gin.New()
 	// Set gin mode to release to avoid logs clutter in terminal
 	gin.SetMode(gin.ReleaseMode)
+
+	// To initialize router with gin
+	router := gin.New()
+
 	// Trusting all proxies on default host
 	router.SetTrustedProxies([]string{"127.0.0.1"})
 
 	// Loading all HTML glob in template directory
 	router.LoadHTMLGlob("templates/*")
+
 	// Initialized gin logger
 	router.Use(gin.Logger())
 
 	// Route for main page
 	routes.HomeRoute(router)
+
 	// Route where you can search for data
 	routes.SearchRoute(router)
 
